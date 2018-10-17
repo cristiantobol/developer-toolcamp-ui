66de5693082ec171bfbe89740a8fd69540f13609
Matthew Potticary <matthewpotticary@Matthews-MacBook-Pro.local>
2018-10-17 22:14:02 +0100

    More formatting

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,19 +1,22 @@
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
-import App from '../components/App';
-import { fetchRecipesAction, receiveRecipesAction } from '../actions';
+import App from "../components/App";
+import { fetchRecipesAction, receiveRecipesAction } from "../actions";
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchRecipes: () => dispatch(fetchRecipesAction()),
-        receiveRecipes: (recipes) => dispatch(receiveRecipesAction(recipes))
-    }
-}
+  return {
+    fetchRecipes: () => dispatch(fetchRecipesAction()),
+    receiveRecipes: recipes => dispatch(receiveRecipesAction(recipes))
+  };
+};
 
 const mapStateToProps = state => {
-    return {
-        recipes: state.recipes,
-    }
-}
+  return {
+    recipes: state.recipes
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
