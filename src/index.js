c38b66dd232d9f98cef644bdc114fe8939a4674b
Matthew Potticary <matthewpotticary@Matthews-MacBook-Pro.local>
2018-10-17 22:13:16 +0100

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // 3rd Party
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 // Custom
-import recipesReducer from './reducer'
-import AppContainer from './containers/AppContainer';
+import recipesReducer from "./reducer";
+import AppContainer from "./containers/AppContainer";
 
-
-const store = createStore(recipesReducer)
+const store = createStore(recipesReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppContainer />
-    </Provider>
-    , document.getElementById('root'));
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,24 +1,26 @@
 import { FETCH_RECIPES, RECEIVE_RECIPES } from "../actions";
 
 const initalState = {
-    fetching: false,
-    recipes: [],
-}
-
+  fetching: false,
+  recipes: []
+};
 
 const recipesReducer = (state = initalState, action) => {
-    var newState;
-    switch(action.type){
-        case FETCH_RECIPES:
-            newState = Object.assign({}, state, {fetching: true})
-            break;
-        case RECEIVE_RECIPES:
-            newState = Object.assign({}, state, {fetching: false, recipes: action.recipes})
-            break;
-        default:
-            newState = state;
-    }
-    return newState;
-}
+  var newState;
+  switch (action.type) {
+    case FETCH_RECIPES:
+      newState = Object.assign({}, state, { fetching: true });
+      break;
+    case RECEIVE_RECIPES:
+      newState = Object.assign({}, state, {
+        fetching: false,
+        recipes: action.recipes
+      });
+      break;
+    default:
+      newState = state;
+  }
+  return newState;
+};
 
 export default recipesReducer;
